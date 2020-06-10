b79d0af1740964468eb36af8bf5593b454297643
Frederick Ernest <frederick.ernest@gmail.com>
2020-06-10 10:42:41 -0700

    add functioning user input and output, with string broken up into an array
    Co-authored-by: Payne Fulcher <pfulcher26@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,17 @@
-$(document).ready(function() {
+//Bussiness Logic
 
+//for (let i = 0; i < vowelArray.length -1; i ++) {
+// if (vowelArray[i] === "a" || vowelArray[i] === "e" || vowelArray[i] === "i" || vowelArray[i] === "o" || vowelArray[i] === "u")
+//}
+
+//UI Logic
+$(document).ready(function() {
+  $("#vowelForm").submit(function(event) {
+    event.preventDefault();
+    let vowels = $("input#word").val();
+    let lowerCase = vowels.toLowerCase();
+    let vowelArray = lowerCase.split('');
+
+    $(".outPut").text(vowelArray);    
+  });
 });
